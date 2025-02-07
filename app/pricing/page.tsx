86b4d1f31869077ce98213e70bf51692085d09be
Curtis Babin <curtsocal@gmail.com>
2025-02-07 15:01:09 -0800

    Add maintenance-detail page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-export default function AboutPage() {
+export default function PricingPage() {
 
     /*
     const pageTitle = "Pricing";
@@ -13,21 +13,21 @@ export default function AboutPage() {
     return (
       <>
       
-        <h1>Pricing</h1>
+        <h1 className="white-text">Pricing</h1>
         
         <h2>Select from our offerings below:</h2>
         
         <ul className="pricing-container">
-          <li><Link href="/maintenance-detail">Maintenance Detail</Link></li>
-          <li><Link href="/platinum-detail">Platinum Detail</Link></li>
-          <li><Link href="/level-1-interior-detail">Level 1 Interior Detail</Link></li>
-          <li><Link href="/level-2-interior-detail">Level 2 Interior Detail</Link></li>
-          <li><Link href="/one-step-paint-correction">1 Step Paint Correction</Link></li>
-          <li><Link href="/two-step-paint-correction">2 Step Paint Correction</Link></li>
-          <li><Link href="/six-month-ceramic-sealant">6 Month Ceramic Sealant</Link></li>
-          <li><Link href="/three-year-ceramic-coating">3 Year Ceramic Coating</Link></li>
-          <li><Link href="/five-year-ceramic-coating">5 Year Ceramic Coating</Link></li>
-          <li><Link href="/seven-year-ceramic-coating">7 Year Ceramic Coating</Link></li>
+          <li><Link href="/pricing/maintenance-detail">Maintenance Detail</Link></li>
+          <li><Link href="/pricing/platinum-detail">Platinum Detail</Link></li>
+          <li><Link href="/pricing/level-1-interior-detail">Level 1 Interior Detail</Link></li>
+          <li><Link href="/pricing/level-2-interior-detail">Level 2 Interior Detail</Link></li>
+          <li><Link href="/pricing/one-step-paint-correction">1 Step Paint Correction</Link></li>
+          <li><Link href="/pricing/two-step-paint-correction">2 Step Paint Correction</Link></li>
+          <li><Link href="/pricing/six-month-ceramic-sealant">6 Month Ceramic Sealant</Link></li>
+          <li><Link href="/pricing/three-year-ceramic-coating">3 Year Ceramic Coating</Link></li>
+          <li><Link href="/pricing/five-year-ceramic-coating">5 Year Ceramic Coating</Link></li>
+          <li><Link href="/pricing/seven-year-ceramic-coating">7 Year Ceramic Coating</Link></li>
         </ul>
   
       </>
