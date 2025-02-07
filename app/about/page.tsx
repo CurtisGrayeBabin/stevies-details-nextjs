f44577356b6b2bd825db422f6c46516e62480155
Curtis Babin <curtsocal@gmail.com>
2025-02-07 14:02:38 -0800

    Improve About page content + CSS

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,33 +12,32 @@ export default function AboutPage() {
     return (
       <>
       
-        <h1>About Us</h1>
+        <h1 className="white-text">About Us</h1>
         
-        <p>At Stevie's Details, we're more than just a mobile detailing service; we're a symbol of excellence in vehicle care. As proud members of the International Detailers Association (IDA), we bring a wealth of experience and expertise to every detailing project.</p>
+        <p>At Stevie's Details, we're more than just a mobile detailing service; we're a symbol of <span className="gold-text">excellence</span> in vehicle care. As proud members of the <u>International Detailers Association</u> (IDA), we bring a wealth of <span className="gold-text">experience</span> and <span className="gold-text">expertise</span> to every detailing project.</p>
   
         <h2>Our Story</h2>
   
-        <p>Our journey began with a shared love for vehicles and a commitment to providing unmatched service to fellow vehicle enthusiasts. Over the years, we've honed our skills, embracing the latest industry techniques and technologies to deliver exceptional results.</p>
+        <p>Our journey began with a shared love for vehicles and a <span className="gold-text">commitment</span> to providing unmatched <span className="gold-text">service</span> to fellow vehicle enthusiasts. Over the years, we've honed our skills, embracing the latest industry techniques and technologies to deliver <span className="gold-text">exceptional</span> results.</p>
   
         <h2>What Sets Us Apart?</h2>
   
-        <p>We're not your average car wash; we're the standard for premium automotive care. Our detailing services go beyond the surface, paying attention to every detail, from the gleaming finish to the immaculate interior. We use only the finest products and techniques to protect and enhance your vehicle's appearance.</p>
+        <p>We're not your average car wash; we're the standard for <span className="gold-text">premium</span> automotive care. Our detailing services go beyond the surface, paying attention to every detail, from the <span className="gold-text">gleaming</span> finish to the <span className="gold-text">immaculate</span> interior. We use only the finest products and techniques to protect and <span className="gold-text">enhance</span> your vehicle's appearance.</p>
   
         <h2>Why Choose Us?</h2>
   
-        <ul>
-        <li><b>Expertise:</b> <p>Our team comprises skilled detailers who take pride in their craft.</p></li>
-        <li><b>Convenience:</b> <p>As a mobile detailing service, we come to you, saving you time and effort.</p></li>
-        <li><b>Quality Assurance:</b> <p>We are committed to your satisfaction, ensuring every detail meets our high standards.</p></li>
-        <li><b>Passion:</b> <p>We share your love for vehicles, and it reflects in our work.</p></li>
+        <ul className="no-bullet-point">
+        <li><b className="bullet-head">Expertise <span className="gold-text">✓</span></b> <p>Our team comprises <span className="gold-text">skilled</span> detailers who take <span className="gold-text">pride</span> in their craft.</p></li>
+        <li><b className="bullet-head">Convenience <span className="gold-text">✓</span></b> <p>As a <span className="gold-text">mobile</span> detailing service, we come to you, saving you <span className="gold-text">time</span> and effort.</p></li>
+        <li><b className="bullet-head">Quality Assurance <span className="gold-text">✓</span></b> <p>We are <span className="gold-text">committed</span> to your satisfaction, ensuring every detail meets our high <span className="gold-text">standards</span>.</p></li>
+        <li><b className="bullet-head">Passion <span className="gold-text">✓</span></b> <p>We share your <span className="gold-text">love</span> for vehicles, and it reflects in our work.</p></li>
         </ul>
   
         <h2>Join Us on this Journey</h2>
   
-        <p>We invite you to experience the difference with Stevie's Details. Whether you drive a daily commuter or a luxury vehicle, we're here to elevate your car care experience. Contact us today to schedule your next detailing appointment and discover what makes us the go-to choice for quality automotive car detailing.</p>
+        <p>We invite you to experience the difference with Stevie's Details. Whether you drive a <span className="gold-text">daily</span> commuter or a <span className="gold-text">luxury</span> vehicle, we're here to <span className="gold-text">elevate</span> your car care experience. Contact us today to schedule your next detailing appointment and discover what makes us the go-to choice for <span className="gold-text">quality</span> automotive car <span className="gold-text">detailing</span>.</p>
   
-        <p>For more information about our IDA certification, you can visit the International Detailers Association website <a href="https://the-ida.com/" target="_" rel="nofollow,noreferrer">here.</a></p>
-  
+        <p>For more information about our IDA certification, you can visit the <span className="gold-text"><u>International Detailers Association</u></span> (IDA) website <a href="https://the-ida.com/" target="_" rel="nofollow,noreferrer">here.</a></p>
   
       </>
     );
