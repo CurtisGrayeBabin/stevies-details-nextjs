6ecf3865a87256b0d7fc824609df61d87986bd09
Curtis Babin <curtsocal@gmail.com>
2025-03-09 19:48:34 -0700

    Fix settings for publishing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export", // Enables static export mode
+    images: {
+      unoptimized: true, // Since Next.js image optimization requires a server
+    },
+};
 
 export default nextConfig;
