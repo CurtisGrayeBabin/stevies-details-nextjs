0dd897ee818c84a8e3a88fe9a0e0dd7983d2d8af
Curtis Babin <curtsocal@gmail.com>
2025-10-19 11:21:07 -0700

    Remove email link from book button

--- components/BookButton.tsx
+++ components/BookButton.tsx
@@ -4,29 +4,8 @@ import { MouseEventHandler } from "react";
 
 const BookButton = () => {
 
-    const isWithinRestrictedHours = (): boolean => {
-        const now = new Date();
-
-        // Convert current time to Pacific Time (handles both PST & PDT automatically)
-        const pstTime = now.toLocaleString("en-US", {
-          timeZone: "America/Los_Angeles",
-          hour12: false,
-          hour: "2-digit",
-        });
-      
-        const hours = parseInt(pstTime, 10);
-        return hours >= 17 || hours < 8; // 5 PM - 8 AM PST/PDT
-    };
-      
-
     const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
-        if (isWithinRestrictedHours()){
-            // email
-            window.location.href = "mailto:steviesdetailsllc@gmail.com?subject=Hello, I would like to book a detail";
-        } else {
-            // text
-            window.location.href = "sms:+17148009788?body=Hello, I would like to book a detail";
-        }
+        window.location.href = "sms:+17148009788?body=Hello, I would like to book a detail";
     };
 
     return (
