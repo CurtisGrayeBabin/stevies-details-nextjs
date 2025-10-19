1a37b5a893cafec383a97035324897fb595c644e
Curtis Babin <curtsocal@gmail.com>
2025-10-19 11:42:15 -0700

    Set trailing slash to true for exporting to fix 404 link sharing issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export", // Enables static export mode
+    trailingSlash: true,
     images: {
       unoptimized: true, // Since Next.js image optimization requires a server
     },
